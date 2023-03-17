043cd5bad62e7b6124825c2b591def4a4ebf0a82
rgoogle <110@qq.com>
2023-03-17 12:49:52 +0800

    Initial commit

---
+++ src/main/java/com/baiyu/voyagerbus/VoyagerbusApplication.java
@@ -0,0 +1,13 @@
+package com.baiyu.voyagerbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyagerbusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyagerbusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baiyu/voyagerbus/VoyagerbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baiyu.voyagerbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyagerbusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
